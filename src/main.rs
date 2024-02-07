0baa7893c5292ed21f6edc6fa040b30c94de172c
Thomas Timmer <thomas.timmer@bettyblocks.com>
2024-02-07 18:56:32 +0100

    fix: use enum for operations

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
 use librclone::rpc;
 use serde_json::json;
 
+#[derive(Debug)]
+pub enum Operations {
+    FsInfo,
+    List,
+    Stat,
+    Size,
+}
+
+impl std::fmt::Display for Operations {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        let op = match self {
+            Operations::FsInfo => "operations/fsinfo",
+            Operations::List => "operations/list",
+            Operations::Stat => "operations/stat",
+            Operations::Size => "operations/size",
+        };
+
+        write!(f, "{}", op)
+    }
+}
+
+impl Into<String> for Operations {
+    fn into(self) -> String {
+        self.to_string()
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     librclone::initialize();
 
-    println!("{}", noop()?);
-    println!("{}", fsinfo(".")?);
-    println!("{}", list(".", "src")?);
-    println!("{}", size("", "src")?);
-    println!("{}", stat(".", "src")?);
+    println!("noop: {}", serde_json::to_string_pretty(&noop()?)?);
+    println!("fsinfo: {}", serde_json::to_string_pretty(&fsinfo(".")?)?);
+    println!("list: {}", serde_json::to_string_pretty(&list(".", "src")?)?);
+    println!("size: {}", serde_json::to_string_pretty(&size("", "src")?)?);
+    println!("stat: {}", serde_json::to_string_pretty(&stat(".", "src")?)?);
 
     librclone::finalize();
 
@@ -17,45 +44,46 @@ fn main() -> anyhow::Result<()> {
 
 /// "command=noop fs=. -o echo=yes -o blue -a path1 -a path2"
 fn noop() -> anyhow::Result<serde_json::Value> {
-    let out = rpc( "backend/command", json!({"command": "noop", "fs": ".", "arg": [ "path1", "path2" ],   "opt": { "blue": "", "echo": "yes" }}).to_string() ).map_err(|e| anyhow::anyhow!(e))?;
-    let json: serde_json::Value = serde_json::from_str(&out)?;
-    Ok(json)
+    rpc(
+        "backend/command",
+        json!({"command": "noop", "fs": ".", "arg": [ "path1", "path2" ], "opt": { "blue": "", "echo": "yes" }})
+            .to_string(),
+    )
+    .map_err(|e| anyhow::anyhow!(e))
+    .and_then(|out| Ok(serde_json::from_str(&out)?))
 }
 
 fn fsinfo(fs: &str) -> anyhow::Result<serde_json::Value> {
-    rpc("operations/fsinfo", json!({"fs": fs}).to_string())
+    rpc(Operations::FsInfo, json!({"fs": fs}).to_string())
         .map_err(|e| anyhow::anyhow!(e))
         .and_then(|out| Ok(serde_json::from_str(&out)?))
 }
 
 fn list(fs: &str, dir: &str) -> anyhow::Result<serde_json::Value> {
-    let out = rpc(
-        "operations/list",
+    rpc(
+        Operations::List,
         json!({"fs": fs, "remote": dir, "opt": { "recurse": false }}).to_string(),
     )
-    .map_err(|e| anyhow::anyhow!(e))?;
-    let json: serde_json::Value = serde_json::from_str(&out)?;
-    Ok(json)
+    .map_err(|e| anyhow::anyhow!(e))
+    .and_then(|out| Ok(serde_json::from_str(&out)?))
 }
 
 fn stat(fs: &str, dir: &str) -> anyhow::Result<serde_json::Value> {
-    let out = rpc(
-        "operations/stat",
+    rpc(
+        Operations::Stat,
         json!({"fs": fs, "remote": dir, "opt": { "recurse": false }}).to_string(),
     )
-    .map_err(|e| anyhow::anyhow!(e))?;
-    let json: serde_json::Value = serde_json::from_str(&out)?;
-    Ok(json)
+    .map_err(|e| anyhow::anyhow!(e))
+    .and_then(|out| Ok(serde_json::from_str(&out)?))
 }
 
 fn size(fs: &str, dir: &str) -> anyhow::Result<serde_json::Value> {
-    let out = rpc(
-        "operations/size",
+    rpc(
+        Operations::Size,
         json!({"fs": format!("{}{}", fs, dir), "opt": { "recurse": false }}).to_string(),
     )
-    .map_err(|e| anyhow::anyhow!(e))?;
-    let json: serde_json::Value = serde_json::from_str(&out)?;
-    Ok(json)
+    .map_err(|e| anyhow::anyhow!(e))
+    .and_then(|out| Ok(serde_json::from_str(&out)?))
 }
 
 // // not supported
